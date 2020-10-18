74c11f7469912724231d7d45a1a48eabe610f6ce
Iuri <160152488@aluno.unb.br>
2020-10-18 13:27:51 -0300

    adding server, route and filter archives

---
+++ src/filter.js
@@ -0,0 +1,56 @@
+import {members, absences} from './api.js';
+
+export async function FilterAllAbsences () 
+{
+    let list_absences = [];
+    let list_members = [];
+    list_absences = await absences(); 
+    list_members = await members(); 
+    
+
+    let peoples = [];
+    list_absences.forEach(element => {
+         list_members.forEach(member => {
+            // Compare id of the users
+            if(member.userId == element.userId)
+            {
+                // add name and type
+                peoples.push({name : member.name , type: element.type, startDate: element.startDate, endDate: element.endDate}) 
+                return peoples;          
+            }
+        });        
+    });  
+    
+    console.log(peoples)
+}
+
+FilterAllAbsences (); 
+
+export async function FilterAbsencesByType()
+{
+    let list_absences = [];
+    let list_members = [];
+    list_absences = await absences(); 
+    list_members = await members(); 
+
+    let list_absences_filtered = [];
+     
+    let list_sick_filtered = [];
+
+        list_absences.forEach(element => {
+            list_members.forEach(member => {
+            // Compare id of the users
+            if(element.type == "vacation" && member.userId == element.userId)
+            {
+                // add name and type
+                list_absences_filtered.push({ID : element.id ,name : member.name , type: element.type})           
+            }
+            else if(element.type == "sickness" && member.userId == element.userId)
+            {
+                list_sick_filtered.push({ID : element.id,name : member.name , type: element.type});        
+            }
+        });        
+    });        
+    //console.log(list_absences_filtered);
+    //console.log(list_sick_filtered);
+}
---
+++ src/routes.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const routes = express.Router()
+/*
+const MemberController = require('./controllers/MemberController')
+const ProjectController = require('./controllers/AbsenceController')
+
+routes
+    // Members
+    .get('/userId', MemberController.index)
+    .post('/members', MemberController.create)
+    .put('/members', MemberController.update)
+    // Absences
+    .get('/absences', ProjectController.index)
+    .post('/absences', ProjectController.create)
+
+
+module.exports = routes
+*/
